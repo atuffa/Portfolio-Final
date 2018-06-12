160b3d1da7fec292878e44cf4c8db7c9c619365e
atuffa <tuffajr@gmail.com>
2018-06-12 18:44:19 -0500

    edit google maps

--- js/scripts.js
+++ js/scripts.js
@@ -324,7 +324,7 @@ $(function () {
 */
 
 function initMap() {
-	var myLatlng = new google.maps.LatLng(40.773328,-73.960088); // <- Your latitude and longitude
+	var myLatlng = new google.maps.LatLng(44.9778,-93.2650); // <- Your latitude and longitude
 	var styles = [
 	{
 		"featureType": "water",
